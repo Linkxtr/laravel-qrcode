e2fbea668dfcaf15cb0ed34c2cf877ee39830990
khaled sadek <65240054+khaled-sadek@users.noreply.github.com>
2025-10-06 10:01:54 +0300

    Add Simple QrCode compatibility tests and update README for V1 release (#4)
    * Add Simple QrCode compatibility tests and update README for V1 release
    
    * Update compatibility tests and workflow
    
    * Update test configurations and compatibility workflow
    
    - Modified test workflow to use composer test with excluded compatibility tests
    - Updated compatibility test workflow to handle simple-qrcode installation
    - Added compatibility test group to SimpleQrCodeTest.php
    - Updated composer.json test scripts to exclude compatibility tests by default
    
    * Downgrade bacon/bacon-qr-code to ^2.0 for compatibility with simplesoftwareio/simple-qrcode
    
    - Updated composer.json to use bacon/bacon-qr-code ^2.0
    - Simplified compatibility test workflow
    - Fixed typo in QrCode.php encoding constant
    
    * fix compatibility test
    
    * Refactor compatibility tests and fix encoding typo
    
    - Refactored SimpleQrCodeTest.php to use Pest datasets for better maintainability
    - Added Datasets.php with test data for QR code generation
    - Updated compatibility-test.yml with an expanded test matrix
    - Fixed typo in QrCode.php encoding constant
    
    * trying to fix tests

---
+++ tests/Compatibility/SimpleQrCodeTest.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Support\Arr;
+use Linkxtr\QrCode\QrCode as LinkxtrQr;
+use SimpleSoftwareIO\QrCode\Generator as SimpleQr;
+
+test('can generate QR code with same output as Simple QrCode', function (string $content) {
+    $linkxtr = (new LinkxtrQr)->format('svg')->generate($content);
+    $simple = (new SimpleQr)->format('svg')->generate($content);
+
+    expect($linkxtr->toHtml())->toBe($simple->toHtml());
+})->with('content')->group('compatibility');
+
+test('supports same methods as Simple QrCode', function (string $method, mixed $param) {
+    $qrCode = (new LinkxtrQr)->{$method}(...Arr::wrap($param));
+    expect($qrCode)->toBeInstanceOf(LinkxtrQr::class);
+})->with('supported_methods')->group('compatibility');
