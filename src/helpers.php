267e271b5ecd025a5baf62fe10ffc3a928ac6342
Khaled Sadek <khaledsadek286@gmail.com>
2026-02-10 23:18:59 +0200

    Consider a more precise return type than mixed

--- src/helpers.php
+++ src/helpers.php
@@ -1,10 +1,11 @@
 <?php
 
+use Illuminate\Support\HtmlString;
 use Linkxtr\QrCode\Facades\QrCode as QrCodeFacade;
 use Linkxtr\QrCode\Generator;
 
 if (! function_exists('qrcode')) {
-    function qrcode(?string $text = null): mixed
+    function qrcode(?string $text = null): Generator|HtmlString|null
     {
         if ($text === null) {
             return app(Generator::class);
