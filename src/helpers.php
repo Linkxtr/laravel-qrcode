e1bb80c7b8e62bd323c32a6cf3cc71239131461b
Khaled Sadek <khaledsadek286@gmail.com>
2026-05-06 12:52:49 +0300

    fix: minor: qrcode() helper dual return type is ambiguous

--- src/helpers.php
+++ src/helpers.php
@@ -7,6 +7,11 @@ use Linkxtr\QrCode\Facades\QrCode as QrCodeFacade;
 use Linkxtr\QrCode\Generator;
 
 if (! function_exists('qrcode')) {
+    /**
+     * Get the QR Code generator instance or generate a QR code directly.
+     *
+     * @return ($text is null ? Generator : HtmlString)
+     */
     function qrcode(?string $text = null): Generator|HtmlString
     {
         if ($text === null) {
